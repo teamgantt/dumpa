e6d9cb84685a6befd30b375b6ef2b26ab28ae5ee
Brian Scaturro <scaturrob@gmail.com>
2019-12-05 10:01:50 -0500

    fix: support utf8 variants

--- create_test_db.sql
+++ create_test_db.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS dumpa_test_db_123;
-CREATE DATABASE dumpa_test_db_123;
+CREATE DATABASE dumpa_test_db_123 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES on dumpa_test_db_123.* to 'dumpa_test'@'%' identified by 'dumpa_test';
 GRANT REPLICATION CLIENT on *.* to 'dumpa_test'@'%';
 GRANT REPLICATION SLAVE on *.* to 'dumpa_test'@'%';
