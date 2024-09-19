4453ca0d193d91f834a1ab78d1438d8ebc5b7402
Bill Harding <wpharding1@gmail.com>
2024-09-19 13:25:37 -0500

    update to MySQL 8

--- project.clj
+++ project.clj
@@ -9,15 +9,15 @@
                  [org.clojure/core.async "0.6.532"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [mysql/mysql-connector-java "5.1.39"]
-                 [org.clojure/java.jdbc "0.6.1"]
+                 [com.mysql/mysql-connector-j "8.0.33"]
+                 [org.clojure/java.jdbc "0.7.12"]
                  [prismatic/schema "1.0.5"]
-                 [com.github.shyiko/mysql-binlog-connector-java "0.4.1"]
+                 [com.zendesk/mysql-binlog-connector-java "0.29.0"]
                  [manifold "0.1.2"]]
 
   :global-vars {*warn-on-reflection* false}
   :min-lein-version "2.5.0"
-  
+
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
@@ -29,7 +29,7 @@
 
   :deploy-repositories [["clojars" {:sign-releases false :url "https://repo.clojars.org" :username :env/clojars_username :password :env/clojars_password}]
                         ["releases"  {:sign-releases false :url "https://repo.clojars.org" :username :env/clojars_username :password :env/clojars_password}]
-                        ["snapshots" {:sign-releases false :url "https://repo.clojars.org" :username :env/clojars_username :password :env/clojars_password}]]  
+                        ["snapshots" {:sign-releases false :url "https://repo.clojars.org" :username :env/clojars_username :password :env/clojars_password}]]
 
   :profiles {:dev {:source-paths ["config" "dev"]
                    :dependencies [[com.stuartsierra/component "0.2.3"]
